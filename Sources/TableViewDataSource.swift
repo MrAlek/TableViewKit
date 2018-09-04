3dfd6d480ccc767553bcd3bacb85f096e14c434a
Viktor Rutberg <viktor.rutberg@avanza.se>
2018-09-04 14:11:52 +0200

    add support so that TableViewDataSource can have a scrollViewDelegate

--- Sources/TableViewDataSource.swift
+++ Sources/TableViewDataSource.swift
@@ -17,6 +17,7 @@ public class TableViewDataSource: NSObject {
     public fileprivate(set) var sections: [TableViewSection]
     fileprivate weak var tableView: UITableView?
     fileprivate let processingQueue = OperationQueue()
+    public weak var scrollViewDelegate: UIScrollViewDelegate?
 
     /// The designated initializer
     ///
@@ -295,7 +296,64 @@ extension TableViewDataSource: UITableViewDelegate {
             sections[indexPath].pasteAction?(tableView, cell, indexPath)
         }
     }
-    
+
+    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        scrollViewDelegate?.scrollViewWillBeginDragging?(scrollView)
+    }
+
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        scrollViewDelegate?.scrollViewDidScroll?(scrollView)
+    }
+
+    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        scrollViewDelegate?.scrollViewDidEndDragging?(scrollView, willDecelerate: decelerate)
+    }
+
+    public func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        scrollViewDelegate?.scrollViewDidZoom?(scrollView)
+    }
+
+    public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        scrollViewDelegate?.scrollViewWillEndDragging?(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
+    }
+
+    public func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        scrollViewDelegate?.scrollViewWillBeginDecelerating?(scrollView)
+    }
+
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        scrollViewDelegate?.scrollViewDidEndDecelerating?(scrollView)
+    }
+
+    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        scrollViewDelegate?.scrollViewDidEndScrollingAnimation?(scrollView)
+    }
+
+    public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return scrollViewDelegate?.viewForZooming?(in: scrollView)
+    }
+
+    public func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        scrollViewDelegate?.scrollViewWillBeginZooming?(scrollView, with: view)
+    }
+
+    public func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        scrollViewDelegate?.scrollViewDidEndZooming?(scrollView, with: view, atScale: scale)
+    }
+
+    public func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
+        return scrollViewDelegate?.scrollViewShouldScrollToTop?(scrollView) ?? false
+    }
+
+    public func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
+        scrollViewDelegate?.scrollViewDidScrollToTop?(scrollView)
+    }
+
+    public func scrollViewDidChangeAdjustedContentInset(_ scrollView: UIScrollView) {
+        if #available(iOS 11.0, *) {
+            scrollViewDelegate?.scrollViewDidChangeAdjustedContentInset?(scrollView)
+        }
+    }
 }
 
 internal extension Collection {
